df45243fecb58f58de10ed7f8ea6ecee05a88a91
Daiming Yang <yang.daim@northeastern.edu>
2023-12-02 05:00:12 -0500

    demo3

--- src/main/java/com/daiming/demo3/ElasticsearchConfig.java
+++ src/main/java/com/daiming/demo3/ElasticsearchConfig.java
@@ -1,12 +1,5 @@
 package com.daiming.demo3;
 
-import co.elastic.clients.elasticsearch.ElasticsearchClient;
-import co.elastic.clients.json.jackson.JacksonJsonpMapper;
-import co.elastic.clients.transport.ElasticsearchTransport;
-import co.elastic.clients.transport.rest_client.RestClientTransport;
-import org.apache.http.HttpHost;
-import org.elasticsearch.client.RestClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
@@ -19,14 +12,4 @@ public class ElasticsearchConfig extends ElasticsearchConfiguration {
                 .connectedTo("localhost:9200")
                 .build();
     }
-
-//    @Bean
-//    public ElasticsearchClient elasticsearchConfig() {
-//        RestClient restClient = RestClient.builder(new HttpHost(
-//                "localhost", 9200, "http")).build();
-//        ElasticsearchTransport transport = new RestClientTransport(restClient, new JacksonJsonpMapper());
-//        return new ElasticsearchClient(transport);
-//    }
-
-
 }
